1061bdfef9da626475bdf63ff0632aed3b9a3b38
Jesse Bie <biehenjia@gmail.com>
2024-07-23 19:54:02 -0400

    loading

---
+++ loading/loading.js
@@ -0,0 +1,30 @@
+import gsap from '../../node_modules/gsap/all.js'
+
+
+
+function loader() {
+    let loaderItem = document.querySelectorAll(".loader__item");
+
+    console.log(loaderItem)
+    const tl = gsap.timeline();
+    gsap.set(loaderItem, {
+        transformOrigin: "100% 100%",
+        scaleX: 1
+    }),
+
+        tl.to(loaderItem, .5, {
+            scaleX: 0,
+            transformOrigin: "0% 0%",
+            stagger: .02,
+            ease: "power3.out"
+        })
+            
+
+}
+
+window.addEventListener('DOMContentLoaded', () => {
+    setTimeout(() => {
+
+        loader();
+    }, 500);
+});
